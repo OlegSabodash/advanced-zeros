f8900d4c965cfa0fbf88a2b7378de047cc116dc0
unknown <o.sabodash@mail.ru>
2018-09-14 21:12:34 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getsumerosCount(number, base) {
+  var baseOne = base;
+  var y = 0;
+  var sum = 0;
+  var arr = [];
+  var n;
+  for (let i = 2; i <= base; i++) {
+      n = 1;
+      if (baseOne % i == 0) {
+          y = 0;
+          while (baseOne % i == 0) {
+              y++;
+              baseOne = Math.floor(baseOne / i);
+          }
+          
+          while (Math.pow(i, n) < number) {
+              sum += Math.floor(number / Math.pow(i, n));
+              n++;
+          }
+          arr.push(sum / y);
+      }
+      arr.sort(function (a, b) {return a - b})
+  }
+  return Math.floor(arr[0]);
+
 }
